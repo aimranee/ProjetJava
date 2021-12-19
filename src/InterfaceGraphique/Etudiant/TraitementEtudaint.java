04d34d91f2b63566b0045bddef0a271998b94ed8
Aimrane Essakhi <aimrane.essakhi@gmail.com>
2021-12-19 16:25:25 +0100

    update: aimrane

---
+++ src/InterfaceGraphique/Etudiant/TraitementEtudaint.java
@@ -0,0 +1,38 @@
+package InterfaceGraphique.Etudiant;
+
+import Administrative.Etudiant.Etudiants;
+import Dao.IDao;
+import ConnectionOracl.ConnectionOracl;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.List;
+
+public class TraitementEtudaint implements IDao<Etudiants> {
+
+    @Override
+    public boolean createElement(Etudiants O) {
+        return false;
+    }
+
+    @Override
+    public boolean updateElement(Etudiants O) {
+        return false;
+    }
+
+    @Override
+    public boolean suppElement(Etudiants O) {
+        return false;
+    }
+
+    @Override
+    public Etudiants getElement(int id) {
+        return null;
+    }
+
+    @Override
+    public List<Etudiants> getAllElements() {
+        return null;
+    }
+}
