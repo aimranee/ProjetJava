b19d26889fcb311ba05578910da80db4901c6427
Salah Eddine <79339578+SalahEddine-Ghannouch@users.noreply.github.com>
2021-12-25 00:46:12 +0100

    connect.java
    commit from salah  : page db

---
+++ src/ConnectionOracl/Connect.java
@@ -0,0 +1,27 @@
+package ConnectionOracl; 
+import java.sql.*;
+
+public class Connect {
+	
+	private static Connection con;
+	static {
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			System.out.print(" Driver ok | ");
+			con=DriverManager.getConnection("jdbc:mysql://localhost/miniprojetjbm","root","");
+			System.out.println("connexion ok");	
+		} catch (ClassNotFoundException e1) {
+			System.out.println("Problème au niveau du pilote1");
+		}catch (SQLException e) {
+			System.out.println("Problème au niveau du SQL EXCEPTION");
+		}
+	}
+
+	public static Connection getCon(){
+		return con;
+	}
+	
+
+}
+
+
